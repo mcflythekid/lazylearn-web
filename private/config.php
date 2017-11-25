e6622d2a789451a1194dd4b740694af5f004c8ec
mcflykid <odopoc@gmail.com>
2017-11-25 23:38:21 +0700

    Split config

--- private/config.php
+++ private/config.php
@@ -6,14 +6,10 @@
 	
 	
 	
-	
+	require_once __DIR__ . '/database.php';
 	date_default_timezone_set("UTC");
 	$SALT = "CT3fkc2LyRYTk9GNXu9gjW";
 	$ASSET =  "/38";
-	$MYSQL_SERVER = "127.0.0.1";
-	$MYSQL_USER = "lazylearn";
-	$MYSQL_PASS = "laplaplon1";
-	$MYSQL_DB = "lazylearn";
 	$REMEMBER_ME_DAY = 30;
 	$FORGET_PASSWORD_DAY = 1;
 	$LEITNER[1] = 86400;
---
+++ private/database.php
@@ -0,0 +1,5 @@
+<?php
+$MYSQL_SERVER = "localhost";
+$MYSQL_USER = "root";
+$MYSQL_PASS = "";
+$MYSQL_DB = "lazylearn_com";
