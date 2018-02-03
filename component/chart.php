4e4db7dcc24255542e4b4c1f906015cbff23ff05
McFly the Kid <odopoc@gmail.com>
2018-02-04 05:11:44 +0700

    Add sum to chart

--- component/chart.php
+++ component/chart.php
@@ -11,14 +11,20 @@
             google.charts.setOnLoadCallback(()=>{
                 $app.api.get(url).then((r)=>{
                     var data = new google.visualization.DataTable();
+                    var correct = 0;
+                    var timeup = 0;
+                    $.each(r.data, (index, obj)=>{
+                        correct += obj.correct;
+                        timeup += obj.timeup;
+                    });
                     data.addColumn('string','Step name');
-                    data.addColumn('number','Correct');
-                    data.addColumn('number','Timeup');
+                    data.addColumn('number','Timeup (' + timeup + ')');
+                    data.addColumn('number','Correct (' + correct + ')');
                     data.addRows(r.data.length);
                     $.each(r.data, (index, obj)=>{
                         data.setCell(index, 0, obj.stepName);
-                        data.setCell(index, 1, obj.correct);
-                        data.setCell(index, 2, obj.timeup);
+                        data.setCell(index, 1, obj.timeup);
+                        data.setCell(index, 2, obj.correct);
                     });
                     var chart = new google.visualization.ColumnChart(document.getElementById(htmlId));
                     chart.draw(data, options);
@@ -31,7 +37,7 @@
             legend: { position: 'top', maxLines: 3 },
             bar: { groupWidth: '75%' },
             isStacked: true,
-            colors: ['#66cc66',  '#fad163'],
+            colors: ['#fad163', '#66cc66' ],
         };
         return e;
     })({});
