7d8beb7eb3767c7fe088471f30d6961c9b74110e
Marty Nguyen <odopoc@gmail.com>
2018-05-20 11:29:12 +0700

    Add bower

--- core.php
+++ core.php
@@ -2,19 +2,20 @@
 $VERSION = 68 ;
 $CTX = "";
 $TITLE = "Lazylearn";
-date_default_timezone_set("UTC");
+$TIMEZONE = "UTC";
+//////////////////////////////////////////////////////////////////////////////////////
 if (is_dev()){
 	$ASSET =  "/res_" . rand(1,10000);
+	$ENDPOINT = 'http://localhost:8088';
 } else {
 	$ASSET =  "/res_" . $VERSION;
+	$ENDPOINT = 'https://beathim.com:8080/lazylearn-api';
 }
-if (is_dev()){
-    $ENDPOINT = 'http://localhost:8088';
-} else {
-    $ENDPOINT = 'https://beathim.com:8080/lazylearn-api';
-}
+///////////////////////////////////////////////////////////////////////////////////////
+date_default_timezone_set($TIMEZONE);
 require_once __DIR__ . '/component/top.php';
 require_once __DIR__ . '/component/bottom.php';
+////////////////////////////////////////////////////////////////////////////////////////
 function is_dev() {
 	$whitelist = array( '127.0.0.1', 'localhost', 'lazylearn.local' );
 	if( in_array( $_SERVER['SERVER_NAME'], $whitelist) )
@@ -25,3 +26,4 @@ function title($str){
 	global $TITLE;
     $TITLE = $str;
 }
+//////////////////////////////////////////////////////////////////////////////////////////
