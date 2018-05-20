0b29d5457a30055be2824e3fa1339a3d6676f7f8
Marty Nguyen <odopoc@gmail.com>
2018-05-20 14:10:22 +0700

    Update layout

--- component/bottom_public.php
+++ component/bottom_public.php
@@ -1,5 +1,16 @@
 <?php function bottom_public(){ ?>
 
 
+    <!-- Site footer -->
+    <footer class="footer">
+        <p>&copy; 2018 McFly the Kid</p>
+    </footer>
+
+    </div> <!-- /container -->
+
+
+
+    </body>
+    </html>
 
 <?php } ?>
--- component/top_public.php
+++ component/top_public.php
@@ -39,53 +39,15 @@ function top_public(){
             <h3 class="text-muted">Lazylearn</h3>
             <nav>
                 <ul class="nav nav-justified">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Github</a></li>
-                    <li><a href="#">Register</a></li>
-                    <li><a href="#">Login</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/register.php">Register</a></li>
+                    <li><a href="/login.php">Login</a></li>
+                    <li><a href="/forget-password.php">Recovery</a></li>
+                    <li><a href="https://github.com/mcflythekid/lazylearn-api" target="_blank">Github</a></li>
                 </ul>
             </nav>
         </div>
 
-        <!-- Jumbotron -->
-        <div class="jumbotron">
-            <h1>Forget about forgeting!</h1>
-            <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-            <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
-        </div>
 
-        <!-- Example row of columns -->
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Safari bug warning!</h2>
-                <p class="text-danger">As of v9.1.2, Safari exhibits a bug in which resizing your browser horizontally causes rendering errors in the justified nav that are cleared upon refreshing.</p>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-                <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-                <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa.</p>
-                <p><a class="btn btn-primary" href="#" role="button">View details &raquo;</a></p>
-            </div>
-        </div>
-
-        <!-- Site footer -->
-        <footer class="footer">
-            <p>&copy; 2018 McFly the Kid</p>
-        </footer>
-
-    </div> <!-- /container -->
-
-
-
-    </body>
-    </html>
 
 <?php } ?>
