4ab14c6662b76d486c603e5225ada359b87e91c1
Marty Nguyen <odopoc@gmail.com>
2018-05-22 06:50:12 +0700

    Up

--- component/top_public.php
+++ component/top_public.php
@@ -15,17 +15,62 @@ function top_public(){
         <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
         <meta name="description" content="">
         <meta name="author" content="">
-        <link rel="icon" href="../../favicon.ico">
+        <link rel="icon" href="<?=$ASSET?>/img/owl-icon.png">
 
         <title><?=$TITLE?></title>
 
         <!-- Bootstrap core CSS -->
         <link href="../bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
-
-
         <!-- Custom styles for this template -->
         <link href="<?=$ASSET?>/external/justified-nav.css" rel="stylesheet">
+
+
+        <script type="text/javascript" src="//www.gstatic.com/charts/loader.js"></script>
+
+        <!-- jquery -->
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="//code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/start/jquery-ui.min.css">
+
+        <!-- bootstrap -->
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+        <!-- axios -->
+        <script src="//cdnjs.cloudflare.com/ajax/libs/axios/0.17.1/axios.js"></script>
+
+        <!-- Include the Quill library -->
+        <link href="//cdn.quilljs.com/1.3.4/quill.snow.css" rel="stylesheet">
+        <script src="//cdn.quilljs.com/1.3.4/quill.js"></script>
+
+        <!-- bootstrap dialog -->
+        <link href="//cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.7/css/bootstrap-dialog.min.css" rel="stylesheet">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.7/js/bootstrap-dialog.min.js"></script>
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- bootstrap table -->
+        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css">
+        <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
+
+
+        <!-- app -->
+        <link rel="stylesheet" href="<?=$ASSET?>/app.css">
+
+
+        <script>
+            var ctx = '<?=$CTX?>';
+            var endpoint = '<?=$ENDPOINT?>';
+        </script>
+        <script src="<?=$ASSET?>/tool.js"></script>
+        <script src="<?=$ASSET?>/app.js"></script>
+        <script src="<?=$ASSET?>/external/quill-magic-url.min.js"></script>
+        <script src="<?=$ASSET?>/external/image-resize.min.js"></script>
 
     </head>
 
