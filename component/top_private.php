8b03a052236ba8d0358acb7e251a992f8fac9cbe
Marty Nguyen <odopoc@gmail.com>
2018-05-26 11:03:38 +0700

    dfg

--- component/top_private.php
+++ component/top_private.php
@@ -48,7 +48,7 @@
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>Lazy</b></span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Lazy</b>LEARN</span>
+            <span class="logo-lg"><b>Lazylearn</b></span>
         </a>
         <!-- Header Navbar: style can be found in header.less -->
         <nav class="navbar navbar-static-top">
