c5cf840e9952437b2207373e4db3bf42678b0da2
Marty Nguyen <odopoc@gmail.com>
2018-05-26 15:34:00 +0700

    Update layout

--- component/top_private.php
+++ component/top_private.php
@@ -46,16 +46,94 @@
     <!-- Google Font -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-    <style>
-        .main-header .navbar .nav>li>a>.label.fontsize14 {
-            /*font-size: 10px;*/
-        }
-    </style>
+    <!-- jQuery 3 -->
+    <script src="/bower_components/jquery/dist/jquery.min.js"></script>
+    <!-- jQuery UI 1.11.4 -->
+    <script src="/bower_components/jquery-ui/jquery-ui.min.js"></script>
+    <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+    <script>
+        $.widget.bridge('uibutton', $.ui.button);
+    </script>
+    <!-- Bootstrap 3.3.7 -->
+    <script src="/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- Morris.js charts -->
+    <script src="/bower_components/raphael/raphael.min.js"></script>
+    <script src="/bower_components/morris.js/morris.min.js"></script>
+    <!-- Sparkline -->
+    <script src="/bower_components/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+    <!-- jvectormap -->
+    <script src="/bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.min.js"></script>
+    <script src="/bower_components/admin-lte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- jQuery Knob Chart -->
+    <script src="/bower_components/jquery-knob/dist/jquery.knob.min.js"></script>
+    <!-- daterangepicker -->
+    <script src="/bower_components/moment/min/moment.min.js"></script>
+    <script src="/bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!-- datepicker -->
+    <script src="/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+    <!-- Bootstrap WYSIHTML5 -->
+    <script src="/bower_components/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+    <!-- Slimscroll -->
+    <script src="/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="/bower_components/fastclick/lib/fastclick.js"></script>
+    <!-- AdminLTE App -->
+    <script src="/bower_components/admin-lte/dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+    <script src="/bower_components/admin-lte/dist/js/pages/dashboard.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="/bower_components/admin-lte/dist/js/demo.js"></script>
+
+    <!--    ------------------------------------------------------------------------------------------------------------------------>
+    <script type="text/javascript" src="//www.gstatic.com/charts/loader.js"></script>
+
+    <!-- jquery -->
+<!--    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>-->
+<!--    <script src="//code.jquery.com/ui/1.11.3/jquery-ui.min.js"></script>-->
+<!--    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/start/jquery-ui.min.css">-->
+
+    <!-- bootstrap -->
+<!--    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >-->
+<!--    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>-->
+
+    <!-- axios -->
+    <script src="//cdnjs.cloudflare.com/ajax/libs/axios/0.17.1/axios.js"></script>
+
+    <!-- Include the Quill library -->
+    <link href="//cdn.quilljs.com/1.3.4/quill.snow.css" rel="stylesheet">
+    <script src="//cdn.quilljs.com/1.3.4/quill.js"></script>
+
+    <!-- bootstrap dialog -->
+    <link href="//cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.7/css/bootstrap-dialog.min.css" rel="stylesheet">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap3-dialog/1.34.7/js/bootstrap-dialog.min.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- bootstrap table -->
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.1/bootstrap-table.min.js"></script>
+
+
+    <!-- app -->
+    <link rel="stylesheet" href="<?=$ASSET?>/app.css">
 
 
     <script>
+        var ctx = '<?=$CTX?>';
+        var endpoint = '<?=$ENDPOINT?>';
+    </script>
+    <script src="<?=$ASSET?>/tool.js"></script>
+    <script src="<?=$ASSET?>/app.js"></script>
+    <script src="<?=$ASSET?>/external/quill-magic-url.min.js"></script>
+    <script src="<?=$ASSET?>/external/image-resize.min.js"></script>
+
+    <script>
         (()=>{
-            $('#logout').click((event)=>{
+            $(document).on('click', '#logout', function(event){
                 event.preventDefault();
                 $app.logout();
             });
@@ -71,6 +149,9 @@
             }
         })();
     </script>
+
+
+
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
@@ -134,16 +215,21 @@
                         <i class="fa fa-th"></i> <span>Dashboard</span>
                     </a>
                 </li>
-                <li class="treeview">
-                    <a href="#">
+                <li class="">
+                    <a href="/deck.php">
                         <i class="fa fa-folder-open-o"></i> <span>Deck</span>
-                        <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>
                     </a>
-                    <ul class="treeview-menu">
-                        <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> Manage</a></li>
-                        <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Create new deck</a></li>
-                    </ul>
                 </li>
+<!--                <li class="treeview">-->
+<!--                    <a href="#">-->
+<!--                        <i class="fa fa-folder-open-o"></i> <span>Deck</span>-->
+<!--                        <span class="pull-right-container"><i class="fa fa-angle-left pull-right"></i></span>-->
+<!--                    </a>-->
+<!--                    <ul class="treeview-menu">-->
+<!--                        <li><a href="pages/forms/general.html"><i class="fa fa-circle-o"></i> Manage</a></li>-->
+<!--                        <li><a href="pages/forms/advanced.html"><i class="fa fa-circle-o"></i> Create new deck</a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
 
             </ul>
         </section>
