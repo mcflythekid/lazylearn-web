b3d5e1156246fe49caf86aa92b58abf63636dd13
McFly the Kid <odopoc@gmail.com>
2018-07-17 19:56:39 +0700

    Change home icon on breadcrumb

--- component/top_private.php
+++ component/top_private.php
@@ -184,7 +184,7 @@
                 <?php if(is_array($PATHS)) { ?>
                 <ol class="breadcrumb">
 
-                    <li><a href="/dashboard.php"><i class="fa fa-th"></i> Home</a></li>
+                    <li><a href="/dashboard.php"><i class="fa fa-home"></i> Home</a></li>
 
                     <?php foreach($PATHS as $key=>$path) { ?>
                         <?php if ($key < sizeof($PATHS) - 1){ ?>
