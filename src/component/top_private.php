70a737c8a3069d50e0fba4375655b6b60837fec0
Marty McFly <odopoc@gmail.com>
2020-05-02 21:23:05 +0700

    Update text

--- src/component/top_private.php
+++ src/component/top_private.php
@@ -86,7 +86,7 @@
                 <!-- mini logo for sidebar mini 50x50 pixels -->
                 <span class="logo-mini"><b>Lazy</b></span>
                 <!-- logo for regular state and mobile devices -->
-                <span class="logo-lg"><b>Lazylearn</b></span>
+                <span class="logo-lg"><b>LazyLearn</b></span>
             </a>
             <!-- Header Navbar: style can be found in header.less -->
             <nav class="navbar navbar-static-top">
