503bb5b39e8f0b64451930e31cec09c0d8136fdc
Marty McFly <odopoc@gmail.com>
2020-05-10 18:02:14 +0700

    Support vietnamese typing

--- src/deck/learn.php
+++ src/deck/learn.php
@@ -134,6 +134,19 @@ Card();
 
 <script>
 
+function removeAscent (str) {
+  if (str === null || str === undefined) return str;
+  str = str.toLowerCase();
+  str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+  str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+  str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+  str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+  str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+  str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+  str = str.replace(/đ/g, "d");
+  return str;
+}
+
 const isTypeableString = html=>{
     if (!html){ // Blank
         return false;
@@ -150,6 +163,9 @@ const isTypeableString = html=>{
     if(text.indexOf("\n")!=-1){// New line
         return false;
     }
+
+    text = removeAscent(text); // Convert vietnamese
+
     if (!/^[\x00-\x7F]*$/.test(text)){ // None ASCII
         return false;
     }
