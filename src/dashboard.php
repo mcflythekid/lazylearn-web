dc273de0782606c0351aaad35f2fb44982ac9507
Marty McFly <odopoc@gmail.com>
2020-05-13 20:45:59 +0700

    Update UI

--- src/dashboard.php
+++ src/dashboard.php
@@ -31,9 +31,11 @@ Deck();
 <div class="u-center">
     <a style="display: none;" id="one-time-learn" class="btn btn-flat btn-lg btn-warning" href="/deck/learn.php?type=learn&id=all-deck">
         <?= $lang["page.dashboard.btn.one_time_learn"] ?>
+        <span></span>
     </a>
     <a style="display: none;" id="today-one-time-learn" class="btn btn-flat btn-lg btn-warning u-ml-10" href="/deck/learn.php?type=learn&id=all-deck-today">
         <?= $lang["page.dashboard.btn.today_one_time_learn"] ?>
+        <span></span>
     </a>
     <a style="display: none;" id="today-one-time-learn-done" class="btn btn-flat btn-lg btn-success u-ml-10"">
         <?= $lang["page.dashboard.btn.today_one_time_learn.done"] ?>
@@ -52,6 +54,7 @@ Deck();
 
         if (count > 0){
             $("#one-time-learn").show();
+            $("#one-time-learn span").text("(" + count + ")");
         } else {
             $("#go-to-deck").show();
         }
@@ -65,6 +68,7 @@ Deck();
 
         if (count > 0){
             $("#today-one-time-learn").show();
+            $("#today-one-time-learn span").text("(" + count + ")");
             $("#today-one-time-learn-done").hide();
         } else {
             $("#today-one-time-learn").hide();
